e8f46a8593b0c3518e051c46b614b0c7da5480a4
Alexey <112402687+IndiPeak@users.noreply.github.com>
2025-10-04 13:35:19 +0300

    add spring

---
+++ src/main/java/com/example/compshop/CompshopApplication.java
@@ -0,0 +1,12 @@
+package com.example.compshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CompshopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CompshopApplication.class, args);
+	}
+}
